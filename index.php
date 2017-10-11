21dce7fb2cbff821b552d678b689f582b51bab2f
Jesus Arciniega <dev.jesusarc@gmail.com>
2017-10-10 23:13:18 -0600

    Filtrar y exportar inventario

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     <!-- Milligram CSS minified -->
     <link rel="stylesheet" href="https://cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
     <link rel="stylesheet" href="css/master.css">
+    <!-- Exportar y manejar tablas -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.css"/>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.js"></script>
 </head>
 <body>
 
@@ -21,18 +25,12 @@
             </div>
             <div class="column">
                 <div class="column menuOptions">
-                    <a href="in.php">Nueva entrada</a> --
-                    <a href="#!">Exportar</a>
+                    <a href="in.php">Nueva entrada</a>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="column">
-                <form class="searchBox">
-                    <fieldset>
-                        <input type="text" placeholder="Buscar.." id="nameField">
-                    </fieldset>
-                </form>
             </div>
         </div>
     </div>
@@ -42,38 +40,64 @@
     <div class="container">
         <div class="row">
             <div class="column">
-                <?php
-                //tomamos los datos del archivo conexion.php
-                require("connect.php");
-                $sql = "SELECT w.id_product, w.product_name, c.name as categoria, w.location, l.name as location FROM warehouse w LEFT JOIN categories c ON w.category = c.id_category LEFT JOIN locations l ON w.location = l.id_location";
-                //se envia la consulta
-                $result=$mysqli->query($sql);
-                $rows = $result->num_rows;
-                    //se despliega el resultado
-                    echo '<table>';
-                    echo '<thead>';
-                    echo '<tr>';
-                    echo '<th>Código</th>';
-                    echo '<th>Descripción</th>';
-                    echo '<th>Categoría</th>';
-                    echo '<th>Ubicación</th>';
-                    echo '</tr>';
-                    echo '</thead>';
-                    echo '<tbody>';
-                    while($row = mysqli_fetch_assoc($result)){
-                        echo '<tr>';
-                        echo '<td><a href="detail.php?code='.$row['id_product'].'">'.$row['id_product'].'</a></td>';
-                        echo '<td>'.$row['product_name'].'</td>';
-                        echo '<td>'.$row['categoria'].'</td>';
-                        echo '<td>'.$row['location'].'</td>';
-                        echo '</tr>';
-                    }
-                    echo '</tbody>';
-                    echo '</table>';
-                    ?>
-                </div>
+                <table id="grid">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Descripción</th>
+                            <th>Categoría</th>
+                            <th>Ubicación</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //tomamos los datos del archivo conexion.php
+                        require("connect.php");
+                        $sql = "SELECT w.id_product, w.product_name, c.name as categoria, w.location, l.name as location FROM warehouse w LEFT JOIN categories c ON w.category = c.id_category LEFT JOIN locations l ON w.location = l.id_location";
+                        //se envia la consulta
+                        $result=$mysqli->query($sql);
+                        $rows = $result->num_rows;
+                        while($row = mysqli_fetch_assoc($result)){
+                            echo '<tr>';
+                            echo '<td><a href="detail.php?code='.$row['id_product'].'">'.$row['id_product'].'</a></td>';
+                            echo '<td>'.$row['product_name'].'</td>';
+                            echo '<td>'.$row['categoria'].'</td>';
+                            echo '<td>'.$row['location'].'</td>';
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
         </div>
+    </div>
 
-    </body>
-    </html>
+    <script type="text/javascript">
+    $( document ).ready(function() {
+        $('#grid').DataTable({
+            "processing": true,
+            "dom": 'lBfrtip',
+            "buttons": [
+                {
+                    extend: 'collection',
+                    text: 'Exportar',
+                    buttons: [
+                        'copy',
+                        'excel',
+                        'csv',
+                        'pdf',
+                        'print'
+                    ]
+                }
+            ],
+            "oLanguage": {
+                "sSearch": "",
+                "sLengthMenu": "Ver _MENU_ ",
+                'sSearchPlaceholder': 'Buscar...'
+            }
+        });
+    });
+    </script>
+
+</body>
+</html>
