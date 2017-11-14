15bf8ccce2f8fdb8d9b5ab758f9a5685a0f4b37a
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-11-14 11:37:00 -0700

    Logout

--- logout.php
+++ logout.php
@@ -2,7 +2,6 @@
 if(!empty($_POST["logout"])) {
     session_start();
     unset($_SESSION["user_id"]);  // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
-    unset($_SESSION["rol"]);  // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
     header("Location: login.php");
 }
 ?>
