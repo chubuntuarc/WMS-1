44be721ef6a7fef37b85a20e4d560319d7832efd
Jesus Arciniega <arciniega.chubuntu@gmail.com>
2017-11-23 14:55:31 -0700

    Registrar sin llave
    Si no se marca casilla no tiene llave

--- put_keys.php
+++ put_keys.php
@@ -4,14 +4,13 @@ require("connect.php");
 
 
 if ( !empty($_POST["key"]) && is_array($_POST["key"]) ) {
-    echo "<ul>";
     foreach ( $_POST["key"] as $key ) {
             $sql = "UPDATE warehouse SET car_key = 1 WHERE id_product = " . $key;
             $mysqli->query($sql);
      }
-     echo "</ul>";
 }
-
+$sql = "UPDATE warehouse SET car_key = 2 WHERE car_key = 0";
+$mysqli->query($sql);
 header("Location: keys.php");
 
  ?>
